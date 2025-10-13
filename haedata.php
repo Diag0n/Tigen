2a7b588a0602901f95122f37ff4e863556df309d
HrDonerwich <masaveikkoantsa@gmail.com>
2025-10-13 08:33:40 +0300

    Index.php, joka yhdistää databaseen
    Database pitää erikseen importoida, mutta pitäisi toimia

---
+++ haedata.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; // default is empty in XAMPP
+$dbname = "tigen";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
